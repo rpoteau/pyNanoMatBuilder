ec3f0f308d8e998b9f73eb79722ce86e2cb0f0e6
rpoteau <romuald.poteau@univ-tlse3.fr>
2026-05-20 16:10:43 +0200

    Bump version: 0.12.4 → 0.12.5 (Polydispersity III)

--- pyNanoMatBuilder/__init__.py
+++ pyNanoMatBuilder/__init__.py
@@ -3,7 +3,7 @@ pyNanoMatBuilder
 A versatile Python library designed to generate atomic-scale 3D structures of nanoparticles (NPs)
 """
 
-__version__ = "0.12.4"
+__version__ = "0.12.5"
 __last_update__ = "2026-05-20"
 __author__ = "Sara Mokhtari, Romuald Poteau"
 
